83e0de061f77e37c45a1865afb6e10b641fcf845
Ben <bensmith.md@gmail.com>
2020-01-31 00:38:04 -0500

    fixed windows sendKeys path

--- renderer.js
+++ renderer.js
@@ -34,7 +34,7 @@ if (ismac) {
     var winoriginal;
     var ffmpegpath =  appRootDir + '\\bin\\win\\ffmpeg.exe';
     var ffprobepath = appRootDir + '\\bin\\win\\fprobe.exe';
-    var sendkeysbatpath = appRootDir + '\\bin\\win\\endKeys.bat';
+    var sendkeysbatpath = appRootDir + '\\bin\\win\\sendKeys.bat';
     var temporiginal = workdir + '\\temp.mp4';
     var workdir = ostemp + '\\' + maketemp();
     var unixify = require('unixify');
